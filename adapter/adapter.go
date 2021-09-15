50f201a412cd40cc169d818275722e3695675851
weiwenchong14566 <weiwenchong14566@ipalfish.com>
2021-09-15 16:17:15 +0800

    init

---
+++ adapter/adapter.go
@@ -0,0 +1,31 @@
+package adapter
+
+import (
+	"github.com/gogf/gf/os/gcron"
+	"github.com/weiwenchong/quant-mocktrander/pub"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/connectivity"
+	"log"
+)
+
+const PORT = "172.17.0.5:10001"
+
+var (
+	Client pub.MocktraderServiceClient
+	Conn   *grpc.ClientConn
+)
+
+func InitClient() {
+	log.Printf("initClient start")
+	gcron.Add("* * * * * *", func() {
+		var err error
+		if Conn == nil || Conn.GetState() == connectivity.TransientFailure || Conn.GetState() == connectivity.Shutdown {
+			Conn, err = grpc.Dial(PORT, grpc.WithInsecure(), grpc.WithBlock())
+			if err != nil {
+				log.Printf("InitClient quant-order err:%v", err)
+			}
+			Client = pub.NewOrderClient(Conn)
+		}
+	})
+	log.Printf("initClient end")
+}
---
+++ go.mod
@@ -0,0 +1,9 @@
+module github.com/weiwenchong/quant-order
+
+go 1.15
+
+require (
+	github.com/gogf/gf v1.16.6
+	google.golang.org/grpc v1.40.0
+	google.golang.org/protobuf v1.27.1
+)
